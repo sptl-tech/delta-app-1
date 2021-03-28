75ec4ee3563b8488037f26b6697e4d4c9e4cc9aa
Vincent <vsaw30@gmail.com>
2021-03-28 00:53:04 -0700

    Reverted accidental commit of old Navigation screen

--- components/Navigation.js
+++ components/Navigation.js
@@ -7,13 +7,6 @@ export default function Home ({navigation}) {
 
         return(
             <View style={styles.container}>
-            <Text>{'\n'}</Text>
-            <Text>{'\n'}</Text>
-            <Text style={styles.TTLED}>Nav Screen Brah</Text>
-            <Text>{'\n'}</Text>
-            <Button style = {styles.button} color = "red" title = "Bluetooth" onPress = {() => navigation.navigate('Bluetooth')}/>
-            <Text>{'\n'}</Text>
-            <Button color = "red" title = "Timer" onPress = {() => navigation.navigate('Timer')}/>
             <Button style = {styles.button} color = "red" title = "LED Display" onPress = {() => navigation.navigate('Bluetooth')}/>
             <Button style = {styles.button} color = "red" title = "Timer" onPress = {() => navigation.navigate('Timer')}/>
             </View>
@@ -31,20 +24,6 @@ const styles = StyleSheet.create({
         width: 200,
         height: 200,
         resizeMode: 'contain',
-    },
-    TTLED: {
-        color: "white",
-        fontSize: 70,
-        fontFamily: "Times New Roman",
-    },
-    button: {
-        borderBottomWidth: 10, 
-        borderBottomColor: 'white'
-    },
-    openMotto: {
-        color: "white",
-        padding: 70,
-        fontFamily: "Georgia"
     },
     button: {
         position: 'absolute',
