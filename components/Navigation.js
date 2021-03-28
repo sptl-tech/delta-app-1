3e280c4b54bbb98ad0223a609082b5d033ba011d
vsawce <56854473+vsawce@users.noreply.github.com>
2021-03-28 00:25:45 -0700

    Fixed missing curly bracket @ line 48

--- components/Navigation.js
+++ components/Navigation.js
@@ -45,6 +45,7 @@ const styles = StyleSheet.create({
         color: "white",
         padding: 70,
         fontFamily: "Georgia"
+    },
     button: {
         position: 'absolute',
         top: 100,
